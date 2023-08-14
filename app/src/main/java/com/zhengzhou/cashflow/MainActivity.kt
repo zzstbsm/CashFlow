7694a0ce29b7e4ada4524ffa0cb6e93723572a14
Zhou Zheng <zheng1.zhou.1998@gmail.com>
2023-08-14 21:23:27 +0200

    Refactor class Event

--- app/src/main/java/com/zhengzhou/cashflow/MainActivity.kt
+++ app/src/main/java/com/zhengzhou/cashflow/MainActivity.kt
@@ -58,16 +58,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-// Allow to post Toast messages from ViewModels
-open class Event<out T>(private val content: T) {
-    private var hasBeenHandled = false
-    //  Returns the content and prevents its use again.
-    fun getContentIfNotHandled(): T? {
-        return if (hasBeenHandled) {
-            null
-        } else {
-            hasBeenHandled = true
-            content
-        }
-    }
-}
